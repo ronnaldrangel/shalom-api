c1a67b8b063a1679e3ed2fbf40af505e6bc60c01
Ronald <ronald@rangel.pro>
2025-08-13 05:01:30 -0500

    LISTO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,7 +177,7 @@ export default function AgenciasPage() {
             </button>
           </div>
         ) : (
-          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
             {filteredAgencias.map((agencia) => (
               <div key={agencia.ter_id} className="bg-white rounded-lg shadow-sm transition-shadow border">
 
